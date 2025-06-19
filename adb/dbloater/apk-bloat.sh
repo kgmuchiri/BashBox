006ee79ee5776c78f5b1103565cd9b4d91f1bd9e
kgmuchiri <kananagmuchiri>
2025-06-19 19:10:57 +0400

    dbloater

--- adb/dbloater/apk-bloat.sh
+++ adb/dbloater/apk-bloat.sh
@@ -4,4 +4,56 @@
 # Comment and uncomment the desired groups in the run.sh script to select which packages to remove.
 
 # Samsung Applications
-"
+SAMSUNG-APPS=(
+    com.samsung.android.app.settings.bixby
+    com.samsung.android.app.watchmanager
+    com.samsung.android.app.contacts
+    com.samsung.android.smartcallprovider
+    com.samsung.android.app.watchmanagerstub
+    com.samsung.android.messaging
+    com.samsung.android.calendar
+    com.samsung.android.themestore
+    com.samsung.android.smartswitchassistant
+    com.samsung.android.arzone
+    com.samsung.android.stickercenter
+    com.samsung.android.aremojieditor
+    com.samsung.android.app.notes
+    com.samsung.android.themecenter
+    com.samsung.android.aremoji
+    com.sec.android.mimage.avatarstickers
+    com.samsung.android.kidsinstaller
+    com.samsung.android.app.spage
+    com.samsung.android.app.routines
+    com.samsung.android.voc)
+
+#Google Apps
+GOOGLE-APPS=(
+    com.google.android.apps.maps
+    com.google.android.youtube
+    com.google.android.gm
+    com.google.android.apps.docs
+    com.google.android.apps.maps
+)
+
+#Facebook
+FACEBOOK_PKGS=(
+    com.facebook.appmanager
+    com.facebook.system
+    com.facebook.services
+
+)
+
+#Phone functionality applications
+#PHONE-APPS=(
+    #com.sec.phone
+    #com.android.phone
+    #com.samsung.android.app.contacts
+    #com.samsung.android.providers.contacts
+    #com.sec.android.widgetapp.easymodecontactswidget
+#)
+
+#netflix
+NETFLIX=(com.netflix.partner.activation)
+
+#onedrive
+ONEDRIVE=(com.microsoft.skydrive)
--- adb/dbloater/run.sh
+++ adb/dbloater/run.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+#exit on error
+set -e
+
+
+print_logo() {
+    cat << "EOF"
+    ____  ____  _             _            
+    |  _ \| __ )| | ___   __ _| |_ ___ _ __ 
+    | | | |  _ \| |/ _ \ / _\` | __/ _ \ '__|    ADB Debloater
+    | |_| | |_) | | (_) | (_| | ||  __/ |       kgmuchiri
+    |____/|____/|_|\___/ \__,_|\__\___|_|  
+
+EOF
+}
+
+print_logo
+
+
 # Load categorized package lists
 source ./apk-bloat.sh
 
 # Check ADB
 if ! command -v adb &> /dev/null; then
-    echo "❌ ADB not installed."
+    echo "ADB not installed."
     exit 1
 fi
 
 # Check device connection
 if ! adb get-state 1>/dev/null 2>&1; then
-    echo "❌ No device connected."
+    echo "No device connected."
     exit 1
 fi
 
@@ -19,7 +37,7 @@ echo "✅ Device connected."
 
 # Combine desired groups here
 ALL_PACKAGES=(
-
+   com.facebook.appmanager
 )
 
 # Uninstall loop
